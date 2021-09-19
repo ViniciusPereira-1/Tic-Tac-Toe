ecaf8114a6340f78e83ecdf8ed936316c81505d6
Vinicius Pereira <viniciuspereirainf2@gmail.com>
2021-09-19 15:34:58 -0300

    -

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ function filledArray (element){
   return element !== ''
 }
 
-class AreaGame {
+class GameArea {
   constructor(){
     this.inPlay = true;
     this.area = ['', '', '',
@@ -21,7 +21,7 @@ class AreaGame {
   cellClick(index){
    this.area[index] = this.currentPlayer;
    this.checkResult();
-   if (this.inPlay === true ) this.switchCurrentPlayer();
+   if (this.inPlay === true) this.switchCurrentPlayer();
    this.clicks++
  }
 
@@ -29,27 +29,23 @@ class AreaGame {
     if (this.clicks < 4) return // Minimum amount of clicks necessary for a win condition
 
     // ROW check
-    for(let i = 0; i <= 6; i++){
+    for(let i = 0 ; i <= 6 ; i++){
         if (this.area[i] !== '') {
           if (this.area[i] === this.area [i+1] && this.area[i] === this.area[i+2]) {
             this.inPlay = false
-            this.area[i] = 'W'
-            this.area[i+1] = 'W'
-            this.area[i+2] = 'W'
+            this.area[i] = this.area[i+1] = this.area[i+2] = 'W'
+            }
           }
-        }
-        i = i+2;
+        i = i + 2;
      }
 
     // COLUMN check
-    for(let i = 0; i <= 2; i++){
+    for(let i = 0 ; i <= 2 ; i++){
         if (this.area[i] !== '') {
           if (this.area[i] === this.area [i+3] && this.area[i] === this.area[i+6]) {
             this.inPlay = false;
-            this.area[i] = 'W'
-            this.area[i+3] = 'W'
-            this.area[i+6] = 'W'
-          }
+            this.area[i] = this.area[i+3] = this.area[i+6] = 'W'
+            }
         }
      }
 
@@ -57,38 +53,53 @@ class AreaGame {
      if (this.area[4] !== ''){
          if (this.area[0] === this.area[4] && this.area[0] === this.area[8]) {
            this.inPlay = false;
-           this.area[0] = 'W'
-           this.area[4] = 'W'
-           this.area[8] = 'W'
-         }
+           this.area[0] = this.area[4] = this.area[8] = 'W'
+           }
          else if (this.area[2] === this.area[4] && this.area[2] === this.area[6]) {
            this.inPlay = false;
-           this.area[2] = 'W'
-           this.area[4] = 'W'
-           this.area[6] = 'W'
-       }
+           this.area[2] = this.area[4] = this.area[6] = 'W'
+           }
      }
 
     // DRAW check
      if (this.area.every(filledArray) === true && this.inPlay === true) {
        this.isDraw = true;
-       return;
      }
 
      // Change the color of the winning squares
      gameSquare.forEach( square => {
-       if (this.area[square.getAttribute('data-index')] === 'W'){
-         square.style.color = 'green';
-         square.style.opacity = '1';
+       if (this.inPlay === false || this.isDraw === true){
+         if (this.area[square.getAttribute('data-index')] === 'W'){
+           square.style.color = 'green';
+           square.style.opacity = '1';
+         } else {
+           square.style.color = 'grey';
+           square.style.opacity = '0.3';
+        }
        }
-     })
+    })
 
+   }
+
+   gameReset(){
+     ticTacToe.area.fill('');
+
+     gameSquare.forEach (square => {
+       square.textContent = ''
+       square.style.opacity = '0.6'
+       square.style.color = 'black'});
+
+     this.inPlay = true;
+     this.isDraw = false;
+     this.currentPlayer = 'X';
+     this.clicks = 0;
+     result.textContent = `Jogada de ${this.currentPlayer}`;
    }
 
  }
 
 // CREATE
-const ticTacToe = new AreaGame ();
+const ticTacToe = new GameArea();
 const gameSquare = document.querySelectorAll('.game-square');
 const result = document.querySelector('.tic-tac-toe-result p');
 const resetButton = document.querySelector('[data-reset]');
@@ -99,33 +110,19 @@ gameSquare.forEach( square => {
 
     let indexValue = square.getAttribute('data-index')
     if (ticTacToe.area[indexValue] !== '' || ticTacToe.inPlay === false || ticTacToe.isDraw === true) return
-    square.innerText = ticTacToe.currentPlayer;
+    square.textContent = ticTacToe.currentPlayer;
     ticTacToe.cellClick(indexValue);
 
     if (ticTacToe.isDraw === true) {
-      result.innerText = 'Deu velha!';
+      result.textContent = 'Deu velha!';
       return;
     }
 
-    ticTacToe.inPlay === true ? result.innerText = `Jogada de ${ticTacToe.currentPlayer}` :
-                                result.innerText = `Vitória de ${ticTacToe.currentPlayer}!`
+    ticTacToe.inPlay === true ? result.textContent = `Jogada de ${ticTacToe.currentPlayer}` :
+                                result.textContent = `Vitória de ${ticTacToe.currentPlayer}!`
   })
 })
 
 resetButton.addEventListener('click', button => {
-
-  //reset game area
-  ticTacToe.area.fill('');
-
-  gameSquare.forEach( square => {
-    square.innerText = ''
-    square.style.opacity = '0.6'
-    square.style.color = 'black'});
-
-  ticTacToe.inPlay = true;
-  ticTacToe.isDraw = false;
-  ticTacToe.currentPlayer = 'X';
-  ticTacToe.clicks = 0;
-  result.innerText = `Jogada de ${ticTacToe.currentPlayer}`;
-
+  ticTacToe.gameReset();
 })
